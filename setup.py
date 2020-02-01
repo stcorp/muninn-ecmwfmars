5b5f35b477b4ccc3861838e16ccca3fae553ebcb
Sander Niemeijer <svniemeijer@mac.com>
2020-02-01 12:56:29 +0100

    Compatibility with PyPI

--- setup.py
+++ setup.py
@@ -3,5 +3,20 @@ from setuptools import setup
 setup(
     name='muninn-ecmwfmars',
     version='1.0',
+    author="S[&]T",
+    url="https://github.com/stcorp/muninn-ecmwfmars",
+    description="Muninn extension for ECMWF GRIB products from the mars archive",
+    license="BSD",
     py_modules=['muninn_ecmwfmars'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=[
+        "muninn",
+    ]
 )
