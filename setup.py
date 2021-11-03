b4a3121d3c1d3f03dd278376c4917a3d181ff850
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-11-03 17:28:31 +0100

    Update to version 1.1

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='muninn-ecmwfmars',
-    version='1.0',
-    author="S[&]T",
-    url="https://github.com/stcorp/muninn-ecmwfmars",
+    name="muninn-ecmwfmars",
+    version="1.1",
     description="Muninn extension for ECMWF GRIB products from the mars archive",
+    url="https://github.com/stcorp/muninn-ecmwfmars",
+    author="S[&]T",
     license="BSD",
-    py_modules=['muninn_ecmwfmars'],
+    py_modules=["muninn_ecmwfmars"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 2",
@@ -15,8 +15,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering",
+        "Environment :: Plugins",
     ],
-    install_requires=[
-        "muninn",
-    ]
+    install_requires=["muninn"]
 )
